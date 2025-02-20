2951ee97d868a7b5cc28ef92996d4c5dfad28559
srohiit2678 <rohitsikhwal99@gmail.com>
2025-02-20 12:37:58 +0530

    Update UserProjectRequest.java

--- src/java/UserProjectRequest.java
+++ src/java/UserProjectRequest.java
@@ -9,37 +9,35 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class UserProjectRequest extends HttpServlet {
-   public UserProjectRequest() {
-   }
 
-   public void doGet(HttpServletRequest var1, HttpServletResponse var2) throws IOException, ServletException {
-      PrintWriter var3 = var2.getWriter();
-      String var4 = var1.getParameter("projectName");
-      String var5 = var1.getParameter("description");
-      String var6 = var1.getParameter("mentorName");
-      String var7 = var1.getParameter("githubLink");
-      String var8 = var1.getParameter("liveLink");
-      String var9 = var1.getParameter("challenges");
-      String var10 = var1.getParameter("projectName");
-      String var11 = "insert into projects (title,description,student_id,department_id,project_link,tech_stack)  values ('" + var4 + "','" + var5 + "','','" + var6 + "','" + var7 + "','','','" + var10 + "') ";
-      var3.println("<html><body>");
-      boolean var12 = true;
-
+    private String Project_query = null;
+    
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+      PrintWriter out = response.getWriter();
+      String projectName = request.getParameter("projectName");
+      String description = request.getParameter("description");
+      String mentorName = request.getParameter("mentorName");
+      String githubLink = request.getParameter("githubLink");
+      String liveLink = request.getParameter("liveLink");
+      String challenges = request.getParameter("challenges");
+      this.Project_query = "insert into projects (title,description,student_id,department_id,project_link,tech_stack)  values ('" + projectName + "','" + description + "','','" + mentorName + "','" + githubLink + "','','','" + projectName + "') ";
+      out.println("<html><body>");
+      boolean project_Submit = true;
       try {
          Class.forName("com.mysql.cj.jdbc.Driver");
-         Connection var13 = DriverManager.getConnection("jdbc:mysql:///ExploreHub?useSSL=false", "root", "root");
-         if (var12) {
-            var3.println("<center><h1>Your Project submited Successfully Wait For Guid Approve.</h1></center>");
+         Connection connection = DriverManager.getConnection("jdbc:mysql:///ExploreHub?useSSL=false", "root", "root");
+         if (project_Submit) {
+            out.println("<center><h1>Your Project submited Successfully Wait For Guid Approve.</h1></center>");
          } else {
-            var3.println("<center><h1>Your Project Not Submited Cheack Details Or Fill Again.</h1></center>");
+            out.println("<center><h1>Your Project Not Submited Cheack Details Or Fill Again.</h1></center>");
          }
 
-         var13.close();
-      } catch (Exception var14) {
-         var3.println(var14);
+         connection.close();
+      } catch (Exception e) {
+         out.println(e);
       }
 
-      var3.println("</body></html>");
-      var3.close();
+      out.println("</body></html>");
+      out.close();
    }
 }
